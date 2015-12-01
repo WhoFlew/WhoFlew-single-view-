0816ef9c9713bb6980bd6894383f77a4f3ceee05
falhelal <fahad.alhelal@emory.edu>
2015-11-30 21:02:23 -0500

    Update GenView.swift

--- WhoFlew/GenView.swift
+++ WhoFlew/GenView.swift
@@ -20,7 +20,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         super.init(frame: frame)
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
@@ -42,7 +42,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
 
     
     
-    var button_Gen: UIView = UIButton(type: UIButtonType.ContactAdd) as UIView
+    var button_Gen: UIView = UIButton.buttonWithType(UIButtonType.ContactAdd) as! UIView
     
     
     
@@ -50,13 +50,9 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
     var pickerView: UIPickerView!
     //triggers pickerview to replace collectionView and cover textField
     var button_OtherTimes: UIButton!
-    
-    
     //3 labels inside (hour, day, min)
     var labelView_PickerTime: UIView!
-    var labelDays: UILabel!
-    var labelHours: UILabel!
-    var labelMins: UILabel!
+    
     
     
     
@@ -76,7 +72,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         
 
         
-        let pickerFrame = CGRect(x: -25.0, y: 30.0, width: frame.width, height: 162.0)
+        let pickerFrame = CGRect(x: 0.0, y: 30.0, width: frame.width, height: 162.0)
         self.pickerView = UIPickerView(frame: pickerFrame)
         self.pickerView.delegate = self
         self.pickerView.dataSource = self
@@ -91,31 +87,32 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         let pickerLabelHeight: CGFloat = ((self.pickerView.frame.height / 2.0) - (self.pickerView.rowSizeForComponent(0).height / 2.0))
         
         
-        let rectDays = CGRect(x: -5.0, y: 0.0, width: frame.width / 3.0, height: pickerLabelHeight)
+        let rectDays = CGRect(x: 0.0, y: 0.0, width: frame.width / 3.0, height: pickerLabelHeight)
         let rectHours = CGRect(x: frame.width / 3.0, y: 0.0, width: frame.width / 3.0, height: pickerLabelHeight)
         let rectMins = CGRect(x: 2 * (frame.width / 3.0), y: 0.0, width: frame.width / 3.0, height: pickerLabelHeight)
         
-        self.labelDays = UILabel(frame: rectDays)
-        self.labelHours = UILabel(frame: rectHours)
-        self.labelMins = UILabel(frame: rectMins)
+        var labelDays = UILabel(frame: rectDays)
+        var labelHours = UILabel(frame: rectHours)
+        var labelMins = UILabel(frame: rectMins)
         
-        self.labelDays.text = "Days"
-        self.labelDays.textAlignment = NSTextAlignment.Center
+        labelDays.text = "Days:"
+        labelDays.textAlignment = NSTextAlignment.Center
         
-        self.labelHours.text = "Hours"
-        self.labelHours.textAlignment = NSTextAlignment.Center
+        labelHours.text = "Hours:"
+        labelHours.textAlignment = NSTextAlignment.Center
         
-        self.labelMins.text = "Mins"
-        self.labelMins.textAlignment = NSTextAlignment.Center
+        labelMins.text = "Mins:"
+        labelMins.textAlignment = NSTextAlignment.Center
         
         
         
-        self.labelView_PickerTime = UIView(frame: CGRect(x: 20.0, y: 77.0, width: frame.width, height: pickerLabelHeight))
-        self.labelView_PickerTime.backgroundColor = self.appDelegate.allColorsArray[0]
+        self.labelView_PickerTime = UIView(frame: CGRect(x: 0.0, y: 30.0, width: frame.width, height: pickerLabelHeight))
+        self.labelView_PickerTime.backgroundColor = self.appDelegate.allColorsArray[14]
         
-        self.labelView_PickerTime.addSubview(self.labelDays)
-        self.labelView_PickerTime.addSubview(self.labelHours)
-        self.labelView_PickerTime.addSubview(self.labelMins)
+        self.labelView_PickerTime.addSubview(labelDays)
+        self.labelView_PickerTime.addSubview(labelHours)
+        self.labelView_PickerTime.addSubview(labelMins)
+        self.labelView_PickerTime.alpha = 0.7
     }
     
     
@@ -129,22 +126,22 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         self.button_Gen.frame.origin.x = frame.width - 32
         //self.button_Gen.center.y = self.textField.center.y
         self.button_Gen.tintColor = UIColor.whiteColor()
-        
+        self.button_Gen.alpha = 0.7
               
         
         
         //scrolls to extremes on left/right
-        let tapGesture = UITapGestureRecognizer(target: self, action: "tapScrollView:")
+        var tapGesture = UITapGestureRecognizer(target: self, action: "tapScrollView:")
         self.collectionView.addGestureRecognizer(tapGesture)
         
         
         
         
         //offsets the y Origin of the view so it is underneath nav bar
-        let offSetY: CGFloat = (self.appDelegate.navBarSize.height + self.appDelegate.statusBarSize.height) - (180 + frame.height)
+        var offSetY: CGFloat = (self.appDelegate.navBarSize.height + self.appDelegate.statusBarSize.height) - (180 + frame.height)
         self.genView = UIView(frame: CGRect(x: 0.0, y: offSetY, width: frame.width, height: 180 + frame.height))
-        self.backgroundColor = self.appDelegate.allColorsArray[1]
-        
+        self.backgroundColor = self.appDelegate.allColorsArray[14]
+   
         
         
         
@@ -152,7 +149,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         
         
         //indicates time from scrollView
-        let labelFrame = CGRect(x: 0.0, y: 0, width: frame.width, height: 60)
+        var labelFrame = CGRect(x: 0.0, y: 0, width: frame.width, height: 60)
         self.label_Time = UILabel(frame: labelFrame)
         
         self.label_Time.text = "set expiration"
@@ -160,7 +157,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         self.label_Time.lineBreakMode = NSLineBreakMode.ByWordWrapping
         self.label_Time.textAlignment = NSTextAlignment.Center
         
-        self.label_Time.backgroundColor = self.appDelegate.allColorsArray[1]
+        self.label_Time.backgroundColor = self.appDelegate.allColorsArray[14]
         
         
         
@@ -208,7 +205,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
     func lazySetCollectionView() -> UICollectionView {
         
         
-        let collectionFrame = CGRect(x: 0.0, y: 59.0, width: (frame.size.width), height: 30)
+        var collectionFrame = CGRect(x: 0.0, y: 59.0, width: (frame.size.width), height: 30)
         
         
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
@@ -217,7 +214,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
         layout.minimumInteritemSpacing = 0.0
         
-        let collectionView = UICollectionView(frame: collectionFrame, collectionViewLayout: layout)
+        var collectionView = UICollectionView(frame: collectionFrame, collectionViewLayout: layout)
         collectionView.contentSize = CGSize(width: (self.durationLimits.count * 100), height: 30)
         
         
@@ -227,7 +224,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         
         collectionView.registerClass(TimeSlideCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
         
-        collectionView.backgroundColor = self.appDelegate.allColorsArray[2]
+        collectionView.backgroundColor = self.appDelegate.allColorsArray[14]
         collectionView.scrollEnabled = true
         collectionView.indicatorStyle = UIScrollViewIndicatorStyle.White
         
@@ -257,11 +254,11 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
             return 24//Hours
         }
         else {
-            return 60//Minutes
+            return 61//Minutes
         }
     }
     
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         
         return "\(row)"
         
@@ -269,51 +266,60 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        let days: Int = pickerView.selectedRowInComponent(0)
-        let hours: Int = pickerView.selectedRowInComponent(1)
-        let minutes: Int = pickerView.selectedRowInComponent(2)
         
-        //minimum is 5 minutes
-        if days == 0
-            && hours == 0
-            && minutes < 5 {
-            self.pickerView.selectRow(5, inComponent: 2, animated: true)
-        }
+        
+        var days: Double = Double(pickerView.selectedRowInComponent(0))
+        var dayString: String = "Days"
+        
+        var hours: Double = Double(pickerView.selectedRowInComponent(1))
+        var hourString: String = "Hours"
+        
+        var minutes: Double = Double(pickerView.selectedRowInComponent(2))
+        var minuteString: String = "Minutes"
+        
+        
+        
         
         if component == 0 {
-
-            if row == 1 {
-                self.labelDays.text = "Day"
-            }
-            else {
-                self.labelDays.text = "Days"
+            days = Double(row)
+            if days == 1 {
+                dayString = "Day"
             }
         }
             
         else if component == 1 {
-            
-            if row == 1 {
-                self.labelHours.text = "Hour"
-            }
-            else {
-                self.labelHours.text = "Hours"
+            hours = Double(row)
+            if hours == 1 {
+                hourString = "Hour"
             }
         }
             
         else {
-      
-            if row == 1 {
-                self.labelMins.text = "Min"
-            }
-            else {
-                self.labelMins.text = "Mins"
+            minutes = Double(row)
+            if minutes == 1 {
+                hourString = "Minute"
             }
         }
         
 
-        self.duration = Double((days * 24 * 60) + (hours * 60) + (minutes))
-        print(self.duration)
+        self.duration = (days * 24 * 60) + (hours * 60) + (minutes)
+        println(self.duration)
         
+        
+        
+        if days == 0 && hours == 0 {
+            self.label_Time.text = "\(Int(minutes))\n\(minuteString)"
+            
+            if minutes < 5 {
+                pickerView.selectRow(5, inComponent: 2, animated: true)
+            }
+        }
+        else if days == 0 {
+            self.label_Time.text = "\(Int(hours)) \(hourString)\n\(Int(minutes)) \(minuteString)"
+        }
+        else {
+            self.label_Time.text = "\(Int(days)) \(dayString)\nH: \(Int(hours))  M: \(Int(minutes))"
+        }
         
     }
     
@@ -359,7 +365,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        let collectionCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! TimeSlideCollectionViewCell
+        var collectionCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! TimeSlideCollectionViewCell
         
         collectionCell.frame.origin.y = 0
         
@@ -380,18 +386,18 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         
         
         if indexPath.row < 10 {
-            let size = CGSize(width: 75, height: 30)
+            var size = CGSize(width: 75, height: 30)
             return size
             
         }
             
         else if indexPath.row < 17 {
-            let size = CGSize(width: 150, height: 30)
+            var size = CGSize(width: 150, height: 30)
             return size
         }
             
         else {
-            let size = CGSize(width: 300, height: 30)
+            var size = CGSize(width: 300, height: 30)
             return size
         }
         
@@ -421,31 +427,31 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
         //75.0: width of items in sectionOne
         //10: number of items in sectionOne
         //width * numberOfItems(with width)
-        let sectionOne: CGFloat = 75.0 * 10
-        let unitOne: CGFloat = scrollView.contentOffset.x / 75.0
-        let minutes = Int((5 * unitOne) + 20)
+        var sectionOne: CGFloat = 75.0 * 10
+        var unitOne: CGFloat = scrollView.contentOffset.x / 75.0
+        var minutes = Int((5 * unitOne) + 20)
         
         
         //150.0: width of items in sectionTwo
         //7: number of items in sectionTwo
         //-75.0: adjusts for text label which has text in center (half of width[150])
-        let sectionTwo: CGFloat = 150.0 * 7
-        let unitTwo: CGFloat = (scrollView.contentOffset.x - sectionOne - 75.0) / 150.0
-        let hours = Int(round(unitTwo)) + 2
+        var sectionTwo: CGFloat = 150.0 * 7
+        var unitTwo: CGFloat = (scrollView.contentOffset.x - sectionOne - 75.0) / 150.0
+        var hours = Int(round(unitTwo)) + 2
         //minutes divided in units of 12 minutes
-        let unit12: CGFloat = (scrollView.contentOffset.x - sectionOne) / (150.0 / 5.0)
-        let minutesBy12 = (Int(unit12 + 5) % 5) * 12
+        var unit12: CGFloat = (scrollView.contentOffset.x - sectionOne) / (150.0 / 5.0)
+        var minutesBy12 = (Int(unit12 + 5) % 5) * 12
         
         
         //300.0: width of items in sectionThree
         //3: number of items in sectionThree
-        //var sectionThree: CGFloat = 300 * 3
+        var sectionThree: CGFloat = 300 * 3
         //-150.0: adjusts for text label which has text in center (half of width[300])
-        let unitThree: CGFloat = (scrollView.contentOffset.x - sectionOne - sectionTwo - 150.0) / 300.0
-        let days = Int(round(unitThree)) + 1
+        var unitThree: CGFloat = (scrollView.contentOffset.x - sectionOne - sectionTwo - 150.0) / 300.0
+        var days = Int(round(unitThree)) + 1
         //minutes divided in units of 12 minutes
-        let unit30: CGFloat = (scrollView.contentOffset.x - sectionOne - sectionTwo ) / (300.0 / 24.0)
-        let hoursInDay = Int(unit30 % 24.0)
+        var unit30: CGFloat = (scrollView.contentOffset.x - sectionOne - sectionTwo ) / (300.0 / 24.0)
+        var hoursInDay = Int(unit30 % 24.0)
         
         
         
@@ -502,7 +508,7 @@ class GenView: UIView, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollVie
     //then, scroll the view to first/last time option
     func tapScrollView(recognizer: UITapGestureRecognizer) {
         
-        //let viewForGesture = recognizer.view!
+        let viewForGesture = recognizer.view!
         let locationOfTouch = recognizer.locationOfTouch(0, inView: self.collectionView)
         
         
